3f286759f90e7ba8a5c4166511b1a7e418b41f98
pyroesp <nicolaslopezesp@gmail.com>
2019-01-17 23:18:00 +0100

    moved Point struct to bezier files, changed function prototypes so they're not dependent on SDL2, to make it more easy to use another drawing library using the same header

--- Bezier Curve/draw.h
+++ Bezier Curve/draw.h
@@ -1,17 +1,13 @@
 #ifndef _DRAW_H
+#define _DRAW_H
 
 #include <stdint.h>
-#include <math.h>
-#include <SDL2/SDL.h>
 
-typedef struct{
-	float x;
-	float y;
-}Point;
+#include "bezier.h"
 
-// Basic SDL surface drawing functions, google is your friend
-void draw_Pixel(SDL_Surface *s, Point p, uint32_t pixel);
-void draw_Line(SDL_Surface *s, Point start, Point end, uint32_t pixel);
-void draw_Dot(SDL_Surface *s, int size, Point p, uint32_t pixel);
+// Basic drawing functions, google is your friend
+void draw_Pixel(void *surface, Point p, uint32_t pixel);
+void draw_Line(void *surface, Point start, Point end, uint32_t pixel);
+void draw_Dot(void *surface, int size, Point p, uint32_t pixel);
 
 #endif
